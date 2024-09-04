3c49e148da1f35d05e7d6a6d93fad41046b90008
dbgys1127 <dbgys1127@gmail.com>
2024-09-04 23:21:15 +0900

    멤버변수 초기화

--- src/initialVar/Animal.java
+++ src/initialVar/Animal.java
@@ -1,12 +1,19 @@
 package initialVar;
 
+import java.util.List;
+
 public class Animal {
-    String name; // 멤버 변수
-    int height;
-    int legCnt; // 멤버 변수
+    String name; // 참조형 - 초기화 하지 않음
+    int height; // 정수형 - 초기화 하지 않음
+
+    boolean canEnterCafe; // 논리형 - 초기화 하지 않음
+
+    int legCnt = 200; // 정수형 - 초기화 함
+
+    public Animal() {
+    }
 
     // 생성자의 매개변수(파라미터)는 지역변수
-
     public Animal(String name, int height, int legCnt) {
         this.name = name;
         this.height = height;
--- src/initialVar/studyInitial.java
+++ src/initialVar/studyInitial.java
@@ -9,17 +9,24 @@ public class studyInitial {
 
     public static void main(String[] args) {
         List<Animal> animalList = new ArrayList<>(); //지역변수
-        animalList.add(new Animal("dog", 70, 4)); //지역변수
-        animalList.add(new Animal("eagle", 100, 2)); //지역변수
-        animalList.add(new Animal("cat", 60, 4)); //지역변수
+        animalList.add(new Animal("dog", 70, 4)); // 멤버변수 값 할당
+        animalList.add(new Animal("eagle", 100, 2)); // 멤버변수 값 할당
+        animalList.add(new Animal("cat", 60, 4)); // 멤버변수 값 할당
+        animalList.add(new Animal()); // 멤버변수 값 미할당 -> 초기값대로 출력예정
 
-        printAnimalInfo(getLargestLegCntAnimal(animalList));
+        for (Animal animal : animalList) {
+            printAnimalInfo(animal);
+        }
     }
 
     // 매개변수(파라미터)는 지역변수
     public static void printAnimalInfo(Animal a) {
+        System.out.println("===== 동물 정보 =====");
         System.out.println("동물 이름 = "+a.name);
         System.out.println("동물 다리 개수 = "+a.legCnt);
+        System.out.println("동물 키 = "+a.height);
+        System.out.println("카페 입장가능 여부 = "+a.canEnterCafe);
+        System.out.println();
     }
 
     // 매개변수(파라미터)는 지역변수
